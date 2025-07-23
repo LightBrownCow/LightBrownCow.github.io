fad2338cb6062ef8beec3e988a8d336a9bc2f81e
LightBrownCow <222216575+LightBrownCow@users.noreply.github.com>
2025-07-23 15:43:23 +0200

    feat: add pwa config

---
+++ public/service-worker.js
@@ -0,0 +1,68 @@
+const CACHE_NAME = `pwa-shell-v-20250723`;
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon.png",
+  "/apple-touch-icon.png",
+  "/apple-touch-icon-precomposed.png",
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  console.log("Installing service worker and caching files...");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching:", FILES_TO_CACHE);
+      return cache.addAll(FILES_TO_CACHE);
+    }),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        }),
+      ),
+    ),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then((cached) => cached || caches.match("/index.html")),
+    );
+    return;
+  }
+
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      const cached = await cache.match(event.request);
+      if (cached) return cached;
+
+      try {
+        const response = await fetch(event.request);
+        if (
+          event.request.method === "GET" &&
+          event.request.url.startsWith(self.location.origin + "/assets/")
+        ) {
+          cache.put(event.request, response.clone());
+        }
+        return response;
+      } catch {
+        return caches.match("/index.html");
+      }
+    })(),
+  );
+});
